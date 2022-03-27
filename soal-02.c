07d30474bccac72ce9685d14b50cd32b772ef694
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-27 21:34:09 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -20,11 +20,112 @@ struct buffer {
 
 int main() {
 	// Input jumlah elemen
+	int n;
 	printf("Masukkan jumlah elemen: ");
-	scanf("%d", ...);
+	scanf("%d", &n);
 
+	// Inisialisasi head dan tail dari masing-masing linked list
+	struct buffer* head1; head1 = (struct buffer*)malloc(sizeof(struct buffer));
+	struct buffer* head2; head2 = (struct buffer*)malloc(sizeof(struct buffer));
+	struct buffer* tail1; tail1 = (struct buffer*)malloc(sizeof(struct buffer));
+	struct buffer* tail2; tail2 = (struct buffer*)malloc(sizeof(struct buffer));
 
-	//template output
-	printf("Hasil: %d", ...);
+	// Temporary variables, untuk passing pointer
+	struct buffer* temp1 = head1; struct buffer* temp2 = head2;
+	struct buffer* temp3 = tail1; struct buffer* temp4 = tail2;
+
+	temp3->next = temp1;
+	temp4->next = temp2;
+
+	// Pengisian LL pertama
+	printf("Masukkan elemen linked list pertama: ");
+	int i = 0;
+	for (i=0; i<n-1; ++i) {
+		temp1->prev = temp3;
+		scanf("%d", &(temp1->data));
+		if (i != n-2) {
+			temp1->next = (struct buffer*)malloc(sizeof(struct buffer));
+		}
+		else {
+			temp1->next = tail1;
+		}
+		temp1 = temp1->next;
+		temp3 = temp3->next;
+	}
+	scanf("%d", &(temp1->data));
+	temp1->prev = temp3;
+
+	// Pengisian LL kedua
+	printf("Masukkan elemen linked list kedua: ");
+	i = 0;
+	for (i=0; i<n-1; ++i) {
+		temp2->prev = temp4;
+		scanf("%d", &(temp2->data));
+		if (i != n-2) {
+			temp2->next = (struct buffer*)malloc(sizeof(struct buffer));
+		}
+		else {
+			temp2->next = tail2;
+		}
+		temp2 = temp2->next;
+		temp4 = temp4->next;
+	}
+	scanf("%d", &(temp2->data));\
+	temp2->prev = temp4;
+
+	// Input flip logic
+	printf("Flip linked list kedua: ");
+	int flip = 0;
+	scanf("%d", &flip);
+
+	// Input shift value
+	printf("Shift linked list kedua: ");
+	int shift = 0;
+	scanf("%d", &shift);
+
+	// Penghitungan hasil
+	int res = 0;
+	if (flip == 1) {
+		while (shift != 0) {
+			if (shift > 0) {
+				head2 = head2->prev;
+				temp2 = temp2->prev;
+				shift--;
+			}
+			else {
+				head2 = head2->next;
+				temp2 = temp2->next;
+				shift++;
+			}
+		}
+		temp1 = head1;
+		temp2 = head2;
+		for (i=0; i<n; i++) {
+			res = res + temp1->data*temp2->data;
+			temp1 = temp1->next; temp2 = temp2->prev;
+		}
+	}
+	else if (flip == 0) {
+		while (shift != 0) {
+			if (shift > 0) {
+				head2 = head2->next;
+				temp2 = temp2->next;
+				shift--;
+			}
+			else {
+				head2 = head2->prev;
+				temp2 = temp2->prev;
+				shift++;
+			}
+		}
+		temp1 = head1;
+		temp2 = head2;
+		for (i=0; i<n; i++) {
+			res = res + temp1->data*temp2->data;
+			temp1 = temp1->next; temp2 = temp2->next;
+		}
+	}
+
+	printf("Hasil: %d", res);
 
 }
